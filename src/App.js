dfc5b54ce3123f1084e2568a67cfeb474202b7ef
AnatoliiSoloviov <anatolsoloviov@gmail.com>
2024-06-12 23:57:45 +0200

    add calc files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslisnt no-eval: 0 */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import './App.css'
+import store from "./store"
+
+class App extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      out: '0' 
+    }
+    this.refOutput = React.createRef()
+
+  }
+
+  tapeNumber(value) {
+    let currentValue =value
+    let output = this.refOutput.current
+    this.setState({
+      out: currentValue
+    })
+    
+    if (output.value === '0') {output.value = ''}
+    output.value += currentValue
+  }
+
+  tapeOperations(value) {
+    let output = this.refOutput.current
+    if (value ==='CE') {
+      output.value.length === 1 ? output.value = '0' :
+      output.value = output.value.substring(0, output.value.length -1)
+    } 
+
+    else if (value === 'C') {output.value = '0'}
+    else if (value === '=') { 
+        try {output.value = eval(output.value) 
+          console.log(output.value) 
+          /* Checking for division by 0 */
+          if (output.value === 'Infinity') {
+            output.value = 'Деление на 0'
+            setTimeout(() => {
+              output.value ='0'
+            },  2000)
+          }
+      }
+      catch {
+        output.value = 'Недопустимое значение'
+        setTimeout(() => {
+          output.value = '0'
+        }, 2000)
+      }
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="container">
+        <div className="output">
+          <input ref={this.refOutput}type="text" defaultValue = {this.state.out} /> 
+        </div>
+        <div className = "button">
+          {store.button.map((item, index) => <button
+          key={index}
+            onClick={() => this.tapeNumber(item.val)}
+          >{item.val}</button>)}
+          {store.operations.map((item,index) => <button
+          key={index}
+            onClick={() => this.tapeOperations(item.val)}
+          >{item.val}</button>)}
+        </div>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
